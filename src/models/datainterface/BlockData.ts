10e1b58d8198b18f37116ce420bfbc498db70138
Alfulinku <Alfulinku@gmail.com>
2023-11-22 09:45:50 +0100

    wip

---
+++ src/models/datainterface/BlockData.ts
@@ -0,0 +1,18 @@
+/**
+ * Represents a data for a given block
+ * Should always be linked to a block number somehow
+ */
+export interface BlockData {
+  price: number;
+  slippageMap: SlippageMap;
+}
+
+/**
+ * Represents the slippage map of a token
+ * From 50 to 2000 bps, give the amount of base you can to sell
+ * to reach 'slippageBps'% slippage
+ * the quote field gives the amount of quote received
+ */
+export interface SlippageMap {
+  [slippageBps: string]: { base: number; quote: number };
+}
---
+++ src/utils/Constants.ts
@@ -0,0 +1,43 @@
+import BigNumber from 'bignumber.js';
+
+/**
+ * Where all the files are saved
+ */
+export const DATA_DIR = process.env.DATA_DIR || process.cwd() + '/data';
+
+/**
+ * List of platforms (dexes) that are available for data querying
+ */
+export const PLATFORMS = ['uniswapv2', 'curve', 'uniswapv3', 'sushiswapv2'];
+
+/**
+ * Base slippages we are searching for the risk oracle frontend
+ * Value in percent
+ */
+export const TARGET_SLIPPAGES = [1, 5, 10, 15, 20];
+
+/**
+ * The spans of days we want to export to the risk oracle frontend
+ */
+export const SPANS = [1, 7, 30, 180, 365];
+
+export const BN_1e18 = new BigNumber(10).pow(18);
+
+/**
+ * data source -> uint map
+ * from contract:
+ * enum LiquiditySource {
+        All,
+        UniV2,
+        UniV3,
+        Curve
+    }
+ */
+export const smartLTVSourceMap = {
+  all: 0,
+  uniswapv2: 1,
+  uniswapv3: 2,
+  curve: 3
+};
+
+export const DEFAULT_STEP_BLOCK = 100;
