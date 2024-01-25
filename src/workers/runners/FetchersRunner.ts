8234cbdf10ccd6a9b52a7407d8f5716901c43510
0x3lden <153280243+0x3lden@users.noreply.github.com>
2024-01-25 09:56:48 +0100

    About to remove custom function

--- src/workers/runners/FetchersRunner.ts
+++ src/workers/runners/FetchersRunner.ts
@@ -3,11 +3,33 @@ import { sleep } from '../../utils/Utils';
 import { BaseWorker } from '../BaseWorker';
 import { WorkerConfiguration } from '../configuration/WorkerConfiguration';
 import { UniswapV2Fetcher } from '../fetchers/uniswapv2/UniswapV2Fetcher';
+import * as fs from 'fs';
 
 const RUN_EVERY_MINUTES = 60;
 const fetchersToLaunch: BaseWorker<WorkerConfiguration>[] = [new UniswapV2Fetcher(RUN_EVERY_MINUTES)];
 
+function ReturnStatus(watched_file: string) {
+  const contentOfFile = fs.readFileSync('data/' + watched_file, 'utf-8');
+  if (contentOfFile.length == 0) return undefined;
+  try {
+    const parsed = JSON.parse(contentOfFile);
+    if ('status' in parsed) return parsed.status;
+    else return undefined;
+  } catch (e) {
+    console.log('Parsing of status file failed with ' + e);
+    return undefined;
+  }
+}
+
 async function FetchersRunner() {
+  if (ReturnStatus(SYNC_FILENAMES.FETCHERS_LAUNCHER) == 'done') startMainLoop();
+
+  fs.watch('data/' + SYNC_FILENAMES.FETCHERS_LAUNCHER, (_) => {
+    if (ReturnStatus(SYNC_FILENAMES.FETCHERS_LAUNCHER) == 'done') startMainLoop();
+  });
+}
+
+async function startMainLoop() {
   // eslint-disable-next-line no-constant-condition
   while (true) {
     const start = Date.now();
