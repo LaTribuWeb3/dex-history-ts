9bffc2a0dbf21fa4f1b25b1103c5cd1d054fc896
0x3lden <153280243+0x3lden@users.noreply.github.com>
2024-04-29 16:12:19 +0200

    Extracts workflow in runOnce command

--- src/workers/runners/MetaRunner.ts
+++ src/workers/runners/MetaRunner.ts
@@ -2,12 +2,12 @@ import { ComputersRunner } from './ComputersRunner';
 import { FetchersRunner } from './FetchersRunner';
 
 export class MetaRunner {
-  static run() {
+  static async run() {
     const fetchersRunner = new FetchersRunner();
-    fetchersRunner.run();
+    await fetchersRunner.runOnce();
 
     const computerRunner = new ComputersRunner();
-    computerRunner.run();
+    await computerRunner.runOnce();
   }
 }
 
