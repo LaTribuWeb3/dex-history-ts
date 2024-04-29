c5281b6defccbba5e858bb8d876e629528c027e5
0x3lden <153280243+0x3lden@users.noreply.github.com>
2024-04-29 17:06:56 +0200

    Rewrite flow to compute the conf at every run instead of once at the top

--- src/workers/runners/AbstractRunner.ts
+++ src/workers/runners/AbstractRunner.ts
@@ -31,21 +31,29 @@ export class AbstractRunner {
   }
 
   async runOnce() {
-    await Promise.all(this.workersToLaunch.map((fetcher) => fetcher.init()));
     await WaitUntilDone(SYNC_FILENAMES.FETCHERS_LAUNCHER);
     UpdateSyncFile(SYNC_FILENAMES.FETCHERS_LAUNCHER, true);
     for (const fetcherToLaunch of this.workersToLaunch) {
-      if (!(fetcherToLaunch.configuration instanceof EmptyConfiguration)) {
-        console.log(`Starting worker ${fetcherToLaunch.workerName} (${fetcherToLaunch.monitoringName})`);
-        for (let i = 0; i < 10; i++) {
-          try {
-            await fetcherToLaunch.run();
-            break;
-          } catch (error) {
-            const errorMsg = `An exception occurred: ${error}`;
-            await sleep(5000);
-            console.log(errorMsg);
-          }
+      try {
+        await fetcherToLaunch.init();
+      } catch (e) {
+        console.warn('Could not load configuration for worker ' + fetcherToLaunch.monitoringName + '. Skipping it.');
+        continue;
+      }
+      if (fetcherToLaunch.configuration instanceof EmptyConfiguration) {
+        console.warn('Configuration for worker ' + fetcherToLaunch.monitoringName + ' is empty. Skipping it.');
+        continue;
+      }
+
+      console.log(`Starting worker ${fetcherToLaunch.workerName} (${fetcherToLaunch.monitoringName})`);
+      for (let i = 0; i < 10; i++) {
+        try {
+          await fetcherToLaunch.run();
+          break;
+        } catch (error) {
+          const errorMsg = `An exception occurred: ${error}`;
+          await sleep(5000);
+          console.log(errorMsg);
         }
         console.log(`Ending fetcher ${fetcherToLaunch.workerName}`);
       }
