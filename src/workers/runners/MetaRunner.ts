fc26c59a16eef06b57a44ce59ece93c60322666a
0x3lden <153280243+0x3lden@users.noreply.github.com>
2024-05-02 17:44:11 +0200

    Restart MetaRunner after run

--- src/workers/runners/MetaRunner.ts
+++ src/workers/runners/MetaRunner.ts
@@ -1,13 +1,42 @@
+import { sleep } from '../../utils/Utils';
+import { AbstractRunner } from './AbstractRunner';
 import { ComputersRunner } from './ComputersRunner';
 import { FetchersRunner } from './FetchersRunner';
 
 export class MetaRunner {
   static async run() {
-    const fetchersRunner = new FetchersRunner();
-    await fetchersRunner.runOnce();
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      const start = Date.now();
 
-    const computerRunner = new ComputersRunner();
-    await computerRunner.runOnce();
+      await MetaRunner.runOnce();
+
+      const runEndDate = Math.round(Date.now() / 1000);
+      const durationSec = runEndDate - Math.round(start / 1000);
+
+      const sleepTime = 1000 * 60 * AbstractRunner.RUN_EVERY_MINUTES - durationSec * 1000;
+      if (sleepTime > 0) {
+        console.log(`sleeping ${Math.round(sleepTime / 1000)} seconds`);
+        await sleep(sleepTime);
+      }
+    }
+  }
+
+  static async runOnce() {
+    for (let i = 0; i < 10; i++) {
+      try {
+        const fetchersRunner = new FetchersRunner();
+        await fetchersRunner.runOnce();
+
+        const computerRunner = new ComputersRunner();
+        await computerRunner.runOnce();
+        break;
+      } catch (error) {
+        const errorMsg = `An exception occurred: ${error}`;
+        await sleep(5000);
+        console.log(errorMsg);
+      }
+    }
   }
 }
 
