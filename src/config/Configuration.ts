1bca6507c4ebcea2dc47d5169e14faed4cb4fd54
Alfu <Alfulinku@gmail.com>
2024-05-23 17:36:02 +0200

    Update Configuration.ts

--- src/config/Configuration.ts
+++ src/config/Configuration.ts
@@ -4,6 +4,8 @@ import * as WorkerConfiguration from '../workers/configuration/WorkerConfigurati
 import { WorkersConfiguration } from '../workers/configuration/WorkerConfiguration';
 import { TokenList } from '../workers/configuration/TokenData';
 
+const configCache: any = {};
+
 export class Configuration {
   static async loadConfig<T>(fileOrURL: string): Promise<T> {
     // Log(`LoadConfiguration: loading tokens data from ${TOKENS_FILE}`);
@@ -16,33 +18,53 @@ export class Configuration {
     }
   }
 
-  static async getWorkersConfiguration() {
-    const configVersion = 'default';
+  static async getWorkersConfiguration(
+    configVersion = 'ethereum'
+  ): Promise<WorkersConfiguration<WorkerConfiguration.FetcherConfiguration>> {
+    const configKey = `worker-${configVersion}`;
+    if (configCache[configKey]) {
+      return configCache[configKey];
+    }
+
     const workersConfigFile =
       process.env.WORKERS_CONFIG_FILE ||
       `https://raw.githubusercontent.com/LaTribuWeb3/dex-history-ts/main/params/workers.${configVersion}.json`;
     const workers = await Configuration.loadConfig<WorkersConfiguration<WorkerConfiguration.FetcherConfiguration>>(
       workersConfigFile
     );
+
+    configCache[configKey] = workers;
     return workers;
   }
 
-  static async getTokensConfiguration() {
-    const configVersion = 'default';
+  static async getTokensConfiguration(configVersion = 'ethereum'): Promise<TokenList> {
+    const configKey = `tokens-${configVersion}`;
+    if (configCache[configKey]) {
+      return configCache[configKey];
+    }
     const tokensConfigFile =
       process.env.TOKENS_CONFIG_FILE ||
       `https://raw.githubusercontent.com/LaTribuWeb3/dex-history-ts/main/params/tokens.${configVersion}.json`;
-    const workers = await Configuration.loadConfig<TokenList>(tokensConfigFile);
-    return workers;
+    const tokens = await Configuration.loadConfig<TokenList>(tokensConfigFile);
+
+    configCache[configKey] = tokens;
+    return tokens;
   }
 
-  static async getPrecomputersConfiguration(): Promise<WorkerConfiguration.PrecomputersConfiguration> {
-    const configVersion = 'default';
+  static async getPrecomputersConfiguration(
+    configVersion = 'ethereum'
+  ): Promise<WorkerConfiguration.PrecomputersConfiguration> {
+    const configKey = `precomputers-${configVersion}`;
+    if (configCache[configKey]) {
+      return configCache[configKey];
+    }
     const precomputersConfigFile =
       process.env.PRECOMPUTERS_CONFIG_FILE ||
       `https://raw.githubusercontent.com/LaTribuWeb3/dex-history-ts/main/params/precomputers.${configVersion}.json`;
     const precomputers: WorkerConfiguration.PrecomputersConfiguration =
       await Configuration.loadConfig<WorkerConfiguration.PrecomputersConfiguration>(precomputersConfigFile);
+
+    configCache[configKey] = precomputers;
     return precomputers;
   }
 }
