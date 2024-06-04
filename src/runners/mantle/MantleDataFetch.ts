1db527d9763969c68d3f855d108749e0a057971a
0xVorian <33686588+0xVorian@users.noreply.github.com>
2024-06-04 13:07:36 +0200

    cleanup

--- src/runners/mantle/MantleDataFetch.ts
+++ src/runners/mantle/MantleDataFetch.ts
@@ -1,12 +1,6 @@
 import { AgniFinanceFetcher } from '../../workers/fetchers/agni/AgniFinanceFetcher';
-import { BalancerFetcher } from '../../workers/fetchers/balancer/BalancerFetcher';
-import { BalancerPriceFetcher } from '../../workers/fetchers/balancer/BalancerPriceFetcher';
 import { ButterFetcher } from '../../workers/fetchers/butter/butterFetcher';
 import { FusionXFinanceFetcher } from '../../workers/fetchers/fusionx/FusionXFinanceFetcher';
-import { UniswapV3Fetcher } from '../../workers/fetchers/uniswapv3/UniswapV3Fetcher';
-import { UniswapV3PriceFetcher } from '../../workers/fetchers/uniswapv3/UniswapV3PriceFetcher';
-import { AdditionalLiquidityPrecomputer } from '../../workers/precomputer/AdditionalLiquidityPrecomputer';
-import { MedianPrecomputer } from '../../workers/precomputer/MedianPrecomputer';
 import { AbstractRunner } from '../AbstractRunner';
 
 export class MantleDataFetch extends AbstractRunner {
