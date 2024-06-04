47ef0a2540f2a5da600c42087cbbd349884bab13
Alfu <Alfulinku@gmail.com>
2024-06-04 17:40:38 +0200

    Update AgniFinanceFetcher.ts

--- src/workers/fetchers/agni/AgniFinanceFetcher.ts
+++ src/workers/fetchers/agni/AgniFinanceFetcher.ts
@@ -1,7 +1,14 @@
+import { AgniPoolV3__factory } from '../../../contracts/types';
 import { UniswapV3Fetcher } from '../uniswapv3/UniswapV3Fetcher';
+import ethers from 'ethers';
 
 export class AgniFinanceFetcher extends UniswapV3Fetcher {
   constructor(runEveryMinutes: number, configVersion: string) {
     super(runEveryMinutes, configVersion, 'agnifinance', 'Agni Finance Fetcher');
+  }
+
+  override getPairContract(poolAddress: string): ethers.BaseContract {
+    console.log(`getPairContract: getting specific fusion contract for ${poolAddress}`);
+    return AgniPoolV3__factory.connect(poolAddress, this.web3Provider);
   }
 }
