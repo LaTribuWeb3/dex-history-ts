297fdb68ed4ec07cd2378450f25d15697b1b9df1
Alfu <Alfulinku@gmail.com>
2024-06-04 15:47:34 +0200

    adds tokens wip

--- src/runners/mantle/MantleDataFetch.ts
+++ src/runners/mantle/MantleDataFetch.ts
@@ -1,6 +1,7 @@
 import { AgniFinanceFetcher } from '../../workers/fetchers/agni/AgniFinanceFetcher';
 import { ButterFetcher } from '../../workers/fetchers/butter/butterFetcher';
 import { FusionXFinanceFetcher } from '../../workers/fetchers/fusionx/FusionXFinanceFetcher';
+import { MedianPrecomputer } from '../../workers/precomputer/MedianPrecomputer';
 import { AbstractRunner } from '../AbstractRunner';
 
 export class MantleDataFetch extends AbstractRunner {
@@ -14,7 +15,8 @@ export class MantleDataFetch extends AbstractRunner {
       [
         // new AgniFinanceFetcher(AbstractRunner.RUN_EVERY_MINUTES, configVersion),
         // new FusionXFinanceFetcher(AbstractRunner.RUN_EVERY_MINUTES, configVersion),
-        new ButterFetcher(AbstractRunner.RUN_EVERY_MINUTES, configVersion)
+        new ButterFetcher(AbstractRunner.RUN_EVERY_MINUTES, configVersion),
+        new MedianPrecomputer(AbstractRunner.RUN_EVERY_MINUTES, configVersion)
       ],
       mutex,
       shouldWait,
--- src/workers/fetchers/butter/butterFetcher.ts
+++ src/workers/fetchers/butter/butterFetcher.ts
@@ -2,6 +2,6 @@ import { UniswapV3Fetcher } from '../uniswapv3/UniswapV3Fetcher';
 
 export class ButterFetcher extends UniswapV3Fetcher {
   constructor(runEveryMinutes: number, configVersion: string) {
-    super(runEveryMinutes, configVersion, 'butter', 'Butter.xyz Finance Fetcher');
+    super(runEveryMinutes, configVersion, 'butter', 'Butter.xyz Fetcher');
   }
 }
