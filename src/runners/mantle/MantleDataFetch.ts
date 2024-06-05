a6385efcaf6186302f87388531f0775858cb60c2
Alfu <Alfulinku@gmail.com>
2024-06-05 12:01:43 +0200

    fix pairs to median

--- src/runners/mantle/MantleDataFetch.ts
+++ src/runners/mantle/MantleDataFetch.ts
@@ -16,13 +16,13 @@ export class MantleDataFetch extends AbstractRunner {
     super(
       'MantleDataFetch-Runner',
       [
-        new AgniFinanceFetcher(AbstractRunner.RUN_EVERY_MINUTES, configVersion),
-        new AgniFinancePriceFetcher(AbstractRunner.RUN_EVERY_MINUTES, configVersion),
-        new FusionXFinanceFetcher(AbstractRunner.RUN_EVERY_MINUTES, configVersion),
-        new FusionXFinancePriceFetcher(AbstractRunner.RUN_EVERY_MINUTES, configVersion),
-        new ButterFetcher(AbstractRunner.RUN_EVERY_MINUTES, configVersion),
-        new ButterPriceFetcher(AbstractRunner.RUN_EVERY_MINUTES, configVersion)
-        // new MedianPrecomputer(AbstractRunner.RUN_EVERY_MINUTES, configVersion)
+        // new AgniFinanceFetcher(AbstractRunner.RUN_EVERY_MINUTES, configVersion),
+        // new AgniFinancePriceFetcher(AbstractRunner.RUN_EVERY_MINUTES, configVersion),
+        // new ButterFetcher(AbstractRunner.RUN_EVERY_MINUTES, configVersion),
+        // new ButterPriceFetcher(AbstractRunner.RUN_EVERY_MINUTES, configVersion),
+        // new FusionXFinanceFetcher(AbstractRunner.RUN_EVERY_MINUTES, configVersion),
+        // new FusionXFinancePriceFetcher(AbstractRunner.RUN_EVERY_MINUTES, configVersion)
+        new MedianPrecomputer(AbstractRunner.RUN_EVERY_MINUTES, configVersion)
       ],
       mutex,
       shouldWait,
